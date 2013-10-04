4019c1d347f73313a1ada39257aaccf5031fb134
James Trey Nickelsen <tarnasa7@gmail.com>
2013-10-04 13:12:50 -0500

    Added 1-1 camera path following
    Also a silly path texture for the floor

--- Unity/Assets/Scripts/CameraFollowRoute.cs
+++ Unity/Assets/Scripts/CameraFollowRoute.cs
@@ -1,99 +1,297 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
 
 public class CameraFollowRoute : MonoBehaviour {
 	
 	public GameObject target;
 	
-	public int currentNode = 0;
+	public GameObject prevPNode;
+	public GameObject nextPNode;
 	
-	public GameObject[] nodes;
+	private Vector3 goalForward;
+	private Vector3 goalPosition;
 	
 	// Use this for initialization
 	void Start () {
-		nodes = GameObject.FindGameObjectsWithTag("CameraTrack");
+		// Find the nearest pNode
+		if (target != null && prevPNode == null && nextPNode == null)
+		{
+			GameObject curPNode = null;
+			GameObject otherPNode = null;
+			bool otherIsNext = false;
+			
+			float minDis = 123456789.0F;
+			foreach(GameObject pNode in GameObject.FindGameObjectsWithTag("PlayerTrack"))
+			{
+				float dis = Vector3.SqrMagnitude(transform.position - pNode.transform.position);
+				if (dis < minDis)
+				{
+					minDis = dis;
+					curPNode = pNode;
+				}
+			}
+			if (curPNode == null)
+			{
+				// Failed to find a near point (Wow, what a fail)
+				return;	
+			}
+			// Iterate through all next and previous nodes to find nearest
+			minDis = 123456789.0F;
+			foreach (GameObject pNode in curPNode.GetComponent<PTrack>().previousPNodes)
+			{
+				float dis = Vector3.SqrMagnitude(transform.position - pNode.transform.position);
+				if (dis < minDis)
+				{
+					minDis = dis;
+					otherPNode = pNode;
+				}
+			}
+			foreach (GameObject pNode in curPNode.GetComponent<PTrack>().nextPNodes)
+			{
+				float dis = Vector3.SqrMagnitude(transform.position - pNode.transform.position);
+				if (dis < minDis)
+				{
+					minDis = dis;
+					otherPNode = pNode;
+					otherIsNext = true;
+				}
+			}
+			if (otherIsNext)
+			{
+				prevPNode = curPNode;
+				nextPNode = otherPNode;
+			}
+			else
+			{
+				prevPNode = otherPNode;
+				nextPNode = curPNode;
+			}
+		}
+		// We only have a previous node
+		else if (target != null && prevPNode != null)
+		{
+			System.Collections.Generic.List<GameObject> nextPNodes = prevPNode.GetComponent<PTrack>().nextPNodes;
+			nextPNode = calculateNextPNode(prevPNode, nextPNodes, target.transform.position);
+		}
+		// we only have a next node
+		else if (target != null && nextPNode != null)
+		{
+			System.Collections.Generic.List<GameObject> previousPNodes = nextPNode.GetComponent<PTrack>().previousPNodes;
+			prevPNode = calculateNextPNode(nextPNode, previousPNodes, target.transform.position);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (target != null && nodes.Length > 0)
+		if (target != null && nextPNode != null && prevPNode != null)
 		{
-			float currentDis = findSqrDistanceToNode(currentNode);
+			System.Collections.Generic.List<GameObject> nextPNodes = nextPNode.GetComponent<PTrack>().nextPNodes;
+			System.Collections.Generic.List<GameObject> prevPNodes = prevPNode.GetComponent<PTrack>().previousPNodes;
 			
-			if (currentNode <= 0)
+			Vector3 targetPos = target.transform.position;
+			
+			if (nextPNodes.Count == 1)
 			{
-				// First and Last Node
-				if (nodes.Length == 1)
+				// Simple path
+				if (prevPNodes.Count == 1)
 				{
-					transform.position = nodes[currentNode].transform.position;
+					// "Simple" 1 to 1 to 1 to 1 path
+					GameObject prevPrevPNode = prevPNodes[0];
+					GameObject nextNextPNode = nextPNodes[0];
+					
+					Vector3 prevPrevPos = prevPrevPNode.transform.position;
+					Vector3 prevPos = prevPNode.transform.position;
+					Vector3 nextPos = nextPNode.transform.position;
+					Vector3 nextNextPos = nextNextPNode.transform.position;
+					
+					// Take average/midpoint of P-PP and N-P
+					Vector3 prevAvg = ((prevPos - prevPrevPos).normalized
+						+ (nextPos - prevPos).normalized) / 2;
+					prevAvg.y = 0;
+					Vector3 prevBorder = Vector3.Cross(prevAvg, Vector3.down);
+					// Take average/midpoint of N-P and NN-N
+					Vector3 nextAvg = ((nextPos - prevPos).normalized
+						+ (nextNextPos - nextPos).normalized) / 2;
+					nextAvg.y = 0;
+					Vector3 nextBorder = Vector3.Cross(nextAvg, Vector3.down);
+					
+					// Find intersection of borders (if possible)
+					float tPivot = tPlaneAndLine(prevAvg, prevPos, nextBorder, nextPos);
+					Vector3 pivotPos = nextPos + tPivot * nextBorder;
+					
+					// Find intersection of ray cast from pivot to target and our current segment
+					Vector3 pivotRayNormal = Vector3.Cross(targetPos - pivotPos, Vector3.down);
+					float tRay = tPlaneAndLine(pivotRayNormal, targetPos, nextPos - prevPos, prevPos);
+					
+					goalForward = getCNode(prevPNode).transform.forward * (1 - tRay)
+						+ getCNode(nextPNode).transform.forward * tRay;
+					goalPosition = getCNode(prevPNode).transform.position * (1 - tRay)
+						+ getCNode(nextPNode).transform.position * tRay;
+					
+					// Check if the target has moved to another node
+					
+					if (Vector3.Dot (targetPos - nextPos, nextAvg) > 0)
+					{
+						prevPNode = nextPNode;
+						nextPNode = nextNextPNode;
+					}
+					else if (Vector3.Dot (targetPos - prevPos, prevAvg) < 0)
+					{
+						nextPNode = prevPNode;
+						prevPNode = prevPrevPNode;
+					}
 				}
-				// First node
+				// Prev is first node
+				else if (prevPNodes.Count == 0)
+				{
+					GameObject nextNextPNode = nextPNodes[0];
+					
+					Vector3 prevPos = prevPNode.transform.position;
+					Vector3 nextPos = nextPNode.transform.position;
+					Vector3 nextNextPos = nextNextPNode.transform.position;
+					
+					Vector3 nextAvg = ((nextPos - prevPos).normalized
+						+ (nextNextPos - nextPos).normalized) / 2;
+					Vector3 nextBorder = Vector3.Cross(nextAvg, Vector3.up);
+					
+					float tNext = tFromPathAndBorder(prevPos, nextPos, targetPos, nextBorder);
+					
+					tNext = tNext < 0 ? 0 : tNext;
+					
+					goalForward = getCNode(prevPNode).transform.forward * (1 - tNext)
+						+ getCNode(nextPNode).transform.forward * tNext;
+					goalPosition = getCNode(prevPNode).transform.position * (1 - tNext)
+						+ getCNode(nextPNode).transform.position * tNext;
+					
+					// Check if the target has moved to another node
+					
+					// Make sure the normals are level
+					nextAvg.y = 0;
+					
+					if (Vector3.Dot (targetPos - nextPos, nextAvg) > 0)
+					{
+						prevPNode = nextPNode;
+						nextPNode = nextNextPNode;
+					}
+				}
 				else
 				{
-					followLine(nodes[currentNode], nodes[currentNode + 1],	target);
-					float nextDis = findSqrDistanceToNode(currentNode + 1);
-					if (nextDis < currentDis)
+					
+				}
+			}
+			else if (nextPNodes.Count == 0)
+			{
+				// Next is last node
+				if (prevPNodes.Count == 1)
+				{
+					GameObject prevPrevPNode = prevPNodes[0];
+					
+					Vector3 prevPrevPos = prevPrevPNode.transform.position;
+					Vector3 prevPos = prevPNode.transform.position;
+					Vector3 nextPos = nextPNode.transform.position;
+					
+					Vector3 prevAvg = ((prevPos - prevPrevPos).normalized
+						+ (nextPos - prevPos).normalized) / 2;
+					Vector3 prevBorder = Vector3.Cross(prevAvg, Vector3.up);
+					
+					float tPrev = tFromPathAndBorder(prevPos, nextPos, targetPos, prevBorder);
+					
+					tPrev = tPrev > 1 ? 1 : tPrev;
+					
+					goalForward = getCNode(prevPNode).transform.forward * (1 - tPrev)
+						+ getCNode(nextPNode).transform.forward * tPrev;
+					goalPosition = getCNode(prevPNode).transform.position * (1 - tPrev)
+						+ getCNode(nextPNode).transform.position * tPrev;
+					
+					// Check if the target has moved to another node
+					
+					// Make sure the normals are level
+					prevAvg.y = 0;
+					
+					if (Vector3.Dot (targetPos - prevPos, prevAvg) < 0)
 					{
-						currentNode += 1;	
+						nextPNode = prevPNode;
+						prevPNode = prevPrevPNode;
 					}
+				}
+				else
+				{
+					
 				}
 			}
 			else
 			{
-				// Last node
-				if (currentNode >= nodes.Length - 1)
+				
+			}
+			
+			// Smoothly move to target position and orientation
+			transform.position += (goalPosition - transform.position) * 0.5F;
+			transform.forward += (goalForward - transform.forward) * 0.5F;
+		}
+	}
+	
+	float tFromPathAndBorder(Vector3 prev, Vector3 next, Vector3 target, Vector3 border)
+	{
+		Vector3 borderNormal = Vector3.Cross (border, Vector3.up);
+		return (Vector3.Dot (target - prev, borderNormal) / Vector3.Dot(next - prev, borderNormal));
+	}
+	
+	GameObject calculateNextPNode(GameObject previousPNode, 
+		System.Collections.Generic.List<GameObject> nextPNodes, Vector3 target)
+	{
+		if (nextPNodes.Count == 1)
+		{
+			return nextPNodes[0];
+		}
+		else
+		{
+			// Finds the (not-normalised) line between the two paths
+			Vector3 averageOfSplit = (nextPNodes[0].transform.position
+				+ nextPNodes[1].transform.position) - previousPNode.transform.position;
+			// Normal vector pointing to the right of the aos
+			Vector3 aosNormal = Vector3.Cross(averageOfSplit, Vector3.up);
+			// Find out what side nextCNodes are on
+			float side = Vector3.Dot(nextPNodes[0].transform.position, aosNormal);
+			// Find out what side the target is on
+			float targetSide = Vector3.Dot(target, aosNormal);
+			if (side < 0)
+			{
+				if (targetSide < 0)
 				{
-					followLine(nodes[currentNode - 1], nodes[currentNode], target);
-					float prevDis = findSqrDistanceToNode(currentNode - 1);
-					if (prevDis < currentDis)
-					{
-						currentNode -= 1;	
-					}
+					return nextPNodes[0];
 				}
-				// Middle node
 				else
 				{
-					followLine(nodes[currentNode], nodes[currentNode + 1], target);
-					// Two nodes to compare with
-					float prevDis = findSqrDistanceToNode(currentNode - 1);
-					float nextDis = findSqrDistanceToNode(currentNode + 1);
-					if (prevDis < currentDis && prevDis < nextDis)
-					{
-						currentNode -= 1;	
-					}
-					else if (nextDis < currentDis && nextDis < prevDis)
-					{
-						currentNode += 1;
-					}
+					return nextPNodes[1];
+				}
+			}
+			else
+			{
+				if (targetSide > 0)
+				{
+					return nextPNodes[0];
+				}
+				else
+				{
+					return nextPNodes[1];
 				}
 			}
 		}
 	}
 	
+	GameObject getCNode(GameObject pNode)
+	{
+		return pNode.GetComponent<PTrack>().cNode;
+	}
+	
+	float tPlaneAndLine(Vector3 normal, Vector3 pointOnPlane, Vector3 lineDir, Vector3 pointOnLine)
+	{
+		return Vector3.Dot(pointOnPlane - pointOnLine, normal) / (Vector3.Dot(lineDir, normal));
+	}
+	
 	float tPointToLine(Vector3 start, Vector3 end, Vector3 point)
 	{
 		// TODO: cache squared distance on startup
-		return -Vector3.Dot(start - point, end - point) / Vector3.SqrMagnitude(end - start);
-	}
-	
-	void followLine(GameObject start, GameObject end, GameObject point)
-	{
-		float t = tPointToLine(
-			start.transform.position,
-			end.transform.position,
-			point.transform.position);
-		transform.position = Vector3.Lerp(
-			start.transform.position,
-			end.transform.position,
-			t);
-		transform.rotation = Quaternion.Lerp(
-			start.transform.rotation,
-			end.transform.rotation,
-			t);
-	}
-	
-	float findSqrDistanceToNode(int nodeNum)
-	{
-		return Vector3.SqrMagnitude(target.transform.position - nodes[nodeNum].transform.position);
+		return -Vector3.Dot(start - point, end - start) / Vector3.SqrMagnitude(end - start);
 	}
 }
---
+++ Unity/Assets/Scripts/PTrack.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PTrack : MonoBehaviour {
+	public GameObject cNode;
+	public System.Collections.Generic.List<GameObject> nextPNodes;
+	public System.Collections.Generic.List<GameObject> previousPNodes;
+	
+	void Start ()
+	{
+		foreach(GameObject next in nextPNodes)
+		{
+			next.GetComponent<PTrack>().previousPNodes.Add(gameObject);
+		}
+	}
+	
+	void OnDrawGizmos()
+	{
+		if (cNode != null)
+		{
+			Gizmos.color = Color.green;
+			Gizmos.DrawLine(transform.position, cNode.transform.position);
+		}
+		foreach(GameObject next in nextPNodes)
+		{
+			Gizmos.color = Color.white;
+			Gizmos.DrawLine(transform.position, next.transform.position);
+		}
+	}
+}
